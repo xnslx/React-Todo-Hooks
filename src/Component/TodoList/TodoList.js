ed45a7ecdac2175bb49864f65fe8ac6790450dbe
Xian Li <xnslx89@gmail.com>
2020-04-25 23:24:54 -0500

    second commit

--- src/Component/TodoList/TodoList.js
+++ src/Component/TodoList/TodoList.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoListItem from '../TodoListItem/TodoListItem';
 
 const TodoList = () => {
     const todoList = [
         {
             id:1,
-            text: 'pick up my daughter'
+            text: 'pick up my daughter',
+            checked: false
         },
         {
             id:2,
-            text: 'keep learning React'
+            text: 'keep learning React',
+            checked: false
         },
         {
             id:3,
-            text: 'start doing a new React project'
+            text: 'start doing a new React project',
+            checked: false
         },
         {
             id:4,
-            text: 'go shopping'
+            text: 'go shopping',
+            checked: false
         }
     ]
+    const [todo, setTodo] = useState(todoList);
+    console.log(todo)
+    const checkedHandler = () => {
+        setTodo({checked: true})
+    }
     return (
         <div>
             {todoList.map(item => (
-                <TodoListItem key={item.id} id={item.id} todo={item.text}/>
+                <TodoListItem key={item.id} id={item.id} todo={item.text} checked={checkedHandler}/>
             ))}
         </div>
     )
--- src/Component/TodoListItem/TodoListItem.js
+++ src/Component/TodoListItem/TodoListItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 
-const TodoListItem = ({id, todo}) => {
+const TodoListItem = ({id, todo, checked}) => {
     return (
-        <ul key={id}>
+        <ul key={id} style={{textDecoration: 'none', listStyle: 'none'}}>
+            <input 
+                type="checkbox" 
+                onChange={checked}
+            />
             <li>{todo}</li>
+            <button>X</button>
         </ul>
     )
 };
